5cc2881a8b7f6f0892c1874e050aaf425f13b536
JoseCorreaMorales <jose.correax@gmail.com>
2023-11-03 12:07:18 -0600

    rendering name and probability in the dom

--- main.js
+++ main.js
@@ -1,19 +1,33 @@
 let net;
 const imgEl = document.getElementsByClassName("image")[0]; // Selecciona el primer elemento de la colección
-let descEl = document.getElementById("desc");
+let descList = document.getElementById("desc");
 
+
+const descContainer = document.createElement("div");
 async function app() {
     net = await mobilenet.load();
     var result = await net.classify(imgEl);
     console.log(result);
-    descEl.innerHTML = result[0].className;
+    //descEl.innerHTML = result[0].className;
 }
+
 
 async function displayImagePrediction() {
     try {
         var result = await net.classify(imgEl);
-        console.log(result);
-        descEl.innerHTML = JSON.stringify(result);
+        //descEl.innerHTML = JSON.stringify(result);
+        //name1.innerHTML = result[0].className;    
+        let data = JSON.parse(JSON.stringify(result));  
+        console.log(data);
+
+        descContainer.innerHTML = `
+        <div class="prediction">
+            <p class="name"> <span>Nombre</span>: ${data[0].className}</p>
+            <p class="probability"><span>Probabilidad</span>: ${data[1].probability}</p>
+        </div>
+        `;
+
+        descList.appendChild(descContainer);
     } catch (error) {
         console.error(error);
     }
@@ -27,9 +41,5 @@ async function cambiarImagen() {
         displayImagePrediction();
     }
 }
-
-/* 
-[{"className":"seashore, coast, seacoast, sea-coast","probability":0.2575416564941406},{"className":"lakeside, lakeshore","probability":0.07770660519599915},{"className":"monitor","probability":0.05762989819049835}]
-*/
 
 window.onload = app; // Llamar a app() después de cargar la página
