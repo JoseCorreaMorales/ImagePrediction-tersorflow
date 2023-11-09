61dcf401cd8f7d49810aa4031de36839d87075fc
JoseCorreaMorales <jose.correax@gmail.com>
2023-11-09 10:28:51 -0600

    Add KNN classifier for image classification

--- main.js
+++ main.js
@@ -1,15 +1,49 @@
 let net;
 const imgEl = document.getElementsByClassName("image")[0]; // Selecciona el primer elemento de la colección
 let descList = document.getElementById("desc");
-
+const webCamElement = document.getElementById('webcam');
+const classifier = knnClassifier.create();
 
 const descContainer = document.createElement("div");
+
+
+async function addExample(classId){
+    const img = await webcam.capture();
+    const activation = net.infer(img, true);
+
+    classifier.addExample(activation,classId);
+    img.dispose();
+}
+
 async function app() {
     try {
         net = await mobilenet.load();
     } catch (error) {
         console.error("Error al cargar el modelo MobileNet:", error);
         alert("El modelo no ha sido cargado aun.");
+    }
+
+    webcam = await tf.data.webcam(webCamElement);
+
+    while (true) {
+        const img = await webcam.capture();
+        const result = await net.classify(img);
+
+        const activation = net.infer(img, "conv_preds");
+        let result2;
+
+        try {
+            result2 = await classifier.predicClass(activation);
+            const clases = ["Untrainded", "Jose", "Ok", "Rock", "Iphone"];
+            document.getElementById("console2" + clases[result2.label]);
+        } catch (error) {
+            console.log(error, "modelo no configura aún...");
+        }
+
+        document.getElementById("console").innerHTML = 'Prediccion ' + result[0].className + "Probabilidad " + result[0].probability
+        img.dispose();
+
+        await tf.nextFrame();
     }
 }
 
